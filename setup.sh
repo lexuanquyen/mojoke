53f05954960f59dacf2b92ed7184a3b1ecf617af
Viet-Trung Luu <viettrungluu@gmail.com>
2014-10-14 13:14:33 -0700

    Install build deps?

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 
 export PATH="$(pwd)/depot_tools:${PATH}"
 gclient sync
+
+cd src
+sudo build/install-build-deps.sh --no-prompt
