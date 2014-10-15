0b8227a48785a28bbfa03560becf0ed45a8071fe
Viet-Trung Luu <viettrungluu@gmail.com>
2014-10-15 14:57:25 -0700

    Limit test concurrency to 8 (to try to avoid retries).
    (By default, it goes for 32, which is presumably the number of
    cores/threads.)

--- test.sh
+++ test.sh
@@ -4,8 +4,7 @@ set -x
 cd src
 for t in $(grep -v '^#' ../targets); do
   echo "*** $t: Starting at $(TZ='America/Los_Angeles' date)"
-  #if out/gn/$t --test-launcher-jobs=4; then
-  if out/gn/$t; then
+  if out/gn/$t --test-launcher-jobs=8; then
     echo "*** $t: PASSED"
   else
     echo "*** $t: FAILED (will not run any more tests)"
