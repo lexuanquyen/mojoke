231f8db1ed37c9fb835da9f8695a5818d8ab76ec
Viet-Trung Luu <viettrungluu@gmail.com>
2014-10-15 08:46:24 -0700

    Quit when any test fails.

--- test.sh
+++ test.sh
@@ -3,7 +3,13 @@ set -x
 
 cd src
 for t in $(grep -v '^#' ../targets); do
-  echo -n "*** Starting $t: "
-  TZ="America/Los_Angeles" date
-  out/gn/$t --test-launcher-jobs=6
+  echo "*** $t: Starting at $(TZ='America/Los_Angeles' date)"
+  if out/gn/$t --test-launcher-jobs=6; then
+    echo "*** $t: PASSED"
+  else
+    echo "*** $t: FAILED (will not run any more tests)"
+    exit 1
+  fi
 done
+
+exit 0
