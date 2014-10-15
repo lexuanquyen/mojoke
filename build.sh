9a2ad8b709d35e74a56c16fbdf4f9e18be22b5b5
Viet-Trung Luu <viettrungluu@gmail.com>
2014-10-15 15:16:58 -0700

    Try -j8, with link concurrency of 2.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -x
 
 export PATH="$(pwd)/depot_tools:${PATH}"
-export GYP_LINK_CONCURRENCY=4
+export GYP_LINK_CONCURRENCY=2
 cd src
 gn gen out/gn
-ninja -j6 -C out/gn $(grep -v '^#' ../targets)
+ninja -j8 -C out/gn $(grep -v '^#' ../targets)
