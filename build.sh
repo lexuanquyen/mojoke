21670b555a715be479a90da1fc313d6984418c72
Viet-Trung Luu <viettrungluu@gmail.com>
2014-10-15 08:38:51 -0700

    -j 6. Also set test parallelism to 6.

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -x
 export PATH="$(pwd)/depot_tools:${PATH}"
 cd src
 gn gen out/gn
-ninja -j4 -C out/gn $(grep -v '^#' ../targets)
+ninja -j6 -C out/gn $(grep -v '^#' ../targets)
--- test.sh
+++ test.sh
@@ -3,5 +3,7 @@ set -x
 
 cd src
 for t in $(grep -v '^#' ../targets); do
-  out/gn/$t
+  echo -n "*** Starting $t: "
+  TZ="America/Los_Angeles" date
+  out/gn/$t --test-launcher-jobs=6
 done
